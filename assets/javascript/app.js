bac4f0c479cb246881176eac4565581afdd6d53d
blarsonmoore <blarsonmoore@gmail.com>
2018-05-16 15:46:54 -0600

    java update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,3 +15,36 @@ function countdown() {
     timeLeft--;
   }
 }
+
+var allQuestions = [{
+  question: 'What is 1+1?',
+  choices: ['0', '1', '2', '3'],
+  correctAnswer: 2
+},
+{
+  question: 'What is 2+2?',
+  choices: ['1', '2', '3', '4'],
+  correctAnswer: 3
+},
+{
+  question: 'What is 3+3?',
+  choices: ['3', '5', '6', '7'],
+  correctAnswer: 2
+},
+{   
+  question: 'What is 3-3?',
+  choices: ['0', '1', '2', '3'],
+  correctAnswer: 0
+}];
+
+//Reference to tags
+var questionTitle = document.getElementById('mainQuestion');
+var selectionList = document.getElementById('selectionList');
+var nextButton = document.getElementById('nextButton');
+
+//Initiating some variables
+var i = 0;
+var length1 = allQuestions.length;
+var correctAnswer = 0;
+
+function populateQuestion() {}
